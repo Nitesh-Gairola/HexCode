592343921a213a5cdeca09c5231ea1ab219431e5
Nitesh-Gairola <69961695+Nitesh-Gairola@users.noreply.github.com>
2021-12-17 17:09:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Colors from './Components/Colors';
+
+const App = () => {
+  return (
+    <>
+      <Colors />
+    </>
+  )
+}
+
+export default App
